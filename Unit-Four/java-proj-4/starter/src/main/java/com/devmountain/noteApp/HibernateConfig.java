627dee3e8a9b988409642d0f5ff0591774e3fe05
LateNightCode2 <daniel.xie@stonybrook.edu>
2023-07-19 19:23:35 -0400

    added unit 4

---
+++ Unit-Four/java-proj-4/starter/src/main/java/com/devmountain/noteApp/HibernateConfig.java
@@ -0,0 +1,19 @@
+// package com.devmountain.noteApp;
+
+// import org.hibernate.HibernateException;
+// import org.hibernate.SessionFactory;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class HibernateConfig {
+
+// @Bean
+// public SessionFactory getSessionFactory() throws HibernateException {
+// SessionFactory sessionFactory = new
+// org.hibernate.cfg.Configuration().configure("hibernate.cfg.xml")
+// .buildSessionFactory();
+
+// return sessionFactory;
+// }
+// }
