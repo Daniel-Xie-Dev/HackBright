fff14b8d143e13ee6b4e2e3ff5854515eeb8b21a
LateNightCode2 <daniel.xie@stonybrook.edu>
2023-08-28 23:39:24 -0400

    added music player with information and less code

--- Digital-Music-Player/music-player-frontend/src/App.js
+++ Digital-Music-Player/music-player-frontend/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
+import MusicPlayer from "./components/Musicplayer/Musicplayer";
 
 function App() {
     return (
         <Router>
             <div className="main-body">
                 <Sidebar />
-                <header className="App-header"></header>
+
+                <MusicPlayer />
             </div>
         </Router>
     );
--- Digital-Music-Player/music-player-frontend/src/components/Musicplayer/Musicplayer.js
+++ Digital-Music-Player/music-player-frontend/src/components/Musicplayer/Musicplayer.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./MusicPlayer.css";
-import { Button } from "react-bootstrap";
+import "./musicPlayer.css";
 import {
-    ArrowLeftSquare,
-    ArrowRightSquare,
-    Pause,
-    Play,
-    Repeat,
-    Repeat1,
-} from "react-bootstrap-icons";
+    AiOutlineLike,
+    AiFillStepBackward,
+    AiFillStepForward,
+    AiFillPlayCircle,
+    AiFillPauseCircle,
+} from "react-icons/ai";
+
+import { MdReplayCircleFilled } from "react-icons/md";
 import axios from "axios";
 
 const options = {
@@ -23,10 +23,10 @@ const options = {
 
 function MusicPlayer() {
     const audioRef = useRef();
-    const [play, setPlay] = useState(false);
+    const [isPlaying, setIsPlaying] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
-    const [repeat, setRepeat] = useState(false);
+    const [isRepeating, setIsRepeating] = useState(false);
     const [int, setInt] = useState(0);
     const [playlist, setPlaylist] = useState([]);
 
@@ -34,74 +34,61 @@ function MusicPlayer() {
         audioRef.current.volume = e.target.value / 100;
     };
 
-    const handleLoadedMetaData = () => {
-        // console.log(Math.ceil(audioRef.current.duration));
-        console.log("hello");
-        setDuration(Math.floor(audioRef.current.duration));
-
-        // audioRef.current.play();
+    const handlePlayPause = () => {
+        setIsPlaying((value) => {
+            value ? audioRef.current.pause() : audioRef.current.play();
+            return !value;
+        });
     };
 
-    const handleMusicMouseUp = (e) => {
-        setPlay(() => {
-            return true;
+    const handleRepeat = () => {
+        setIsRepeating((value) => {
+            audioRef.current.loop = !value;
+            return !value;
         });
+    };
+
+    const handleMusicMouseDown = () => {
+        setIsPlaying(false);
+        audioRef.current.pause();
+    };
+
+    const handleMusicMouseUp = () => {
+        setIsPlaying(true);
         audioRef.current.play();
     };
 
+    const handleLoadedMetaData = () => {
+        setDuration(Math.floor(audioRef.current.duration));
+    };
+
     const handleMusicTimestamp = (e) => {
-        setPlay(false);
-        audioRef.current.pause();
         audioRef.current.currentTime = parseInt(e.target.value);
         setCurrentTime(parseInt(e.target.value));
         // console.log(e.target.value);
     };
 
     const handleMusicPlaying = () => {
-        setCurrentTime(audioRef.current.currentTime);
+        setCurrentTime(Math.floor(audioRef.current.currentTime));
     };
-
-    const handlePausePlay = () => {
-        play ? audioRef.current.pause() : audioRef.current.play();
-        setPlay(!play);
-    };
-
-    const handleRepeat = () => {
-        audioRef.current.loop = !repeat;
-        setRepeat(!repeat);
-    };
-
-    // useEffect(() => {
-    //     audioRef.current.addEventListener("play", handleTimeStampEvent);
-
-    //     return () => {
-    //         audioRef.current.removeEventListener("play", handleTimeStampEvent);
-    //     };
-    // });
 
     const handleNextPrevious = (index) => {
-        setPlay(() => {
-            return false;
+        setInt((initialValue) => {
+            const playlistIndex =
+                initialValue + index < 0
+                    ? playlist.length - 1
+                    : (initialValue + index) % playlist.length;
+            audioRef.current.src = playlist[playlistIndex].preview;
+            return playlistIndex;
         });
-        if (int + index >= playlist.length) {
-            setInt(0);
-            audioRef.current.src = playlist[0].preview;
-        } else if (int + index < 0) {
-            setInt(playlist.length - 1);
-            audioRef.current.src = playlist[playlist.length - 1].preview;
-        } else {
-            setInt(int + index);
-            audioRef.current.src = playlist[int + index].preview;
-        }
-        // setPlay(true);
     };
 
     useEffect(() => {
         const getSongs = async () => {
             const result = await axios.request(options);
-            // console.log(result.data.data);
             setPlaylist(() => {
                 audioRef.current.src = result.data.data[0].preview;
+                // console.log(result.data.data);
                 return result.data.data;
             });
         };
@@ -110,53 +97,75 @@ function MusicPlayer() {
     }, []);
 
     return (
-        <div className="MusicPlayer">
-            <div className="MusicPlayerContainer">
-                <audio
-                    // autoPlay
-                    className="MusicPlayerAudio"
-                    ref={audioRef}
-                    loop={repeat}
-                    onTimeUpdate={handleMusicPlaying}
-                    onLoadedMetadata={handleLoadedMetaData}
-                    onEnded={() => handleNextPrevious(1)}
-                >
-                    {/* {console.log(playlist)} */}
-                    <source type="audio/mp3"></source>
-                    Your browser does not support audio element.
-                </audio>
-
-                <input
-                    type="range"
-                    min={0}
-                    max={duration}
-                    value={currentTime}
-                    step={1}
-                    onChange={handleMusicTimestamp}
-                    onMouseUp={handleMusicMouseUp}
-                ></input>
-                <input
-                    type="range"
-                    min={0}
-                    max={100}
-                    defaultValue={100}
-                    onChange={(e) => handleVolumeChange(e)}
-                ></input>
-                <Button onClick={() => handleNextPrevious(-1)}>
-                    <ArrowLeftSquare></ArrowLeftSquare>
-                </Button>
-
-                <Button onClick={handlePausePlay}>
-                    {play ? <Pause></Pause> : <Play></Play>}
-                </Button>
-
-                <Button onClick={() => handleNextPrevious(1)}>
-                    <ArrowRightSquare></ArrowRightSquare>
-                </Button>
-
-                <Button onClick={handleRepeat}>
-                    {repeat ? <Repeat1></Repeat1> : <Repeat></Repeat>}
-                </Button>
+        <div className="music-player">
+            <div className="song-bar">
+                <div className="song-infos">
+                    {playlist.length !== 0 ? (
+                        <>
+                            <div className="image-container">
+                                <img src={playlist[int].album.cover} alt="" />
+                            </div>
+                            <div className="song-description">
+                                <p className="song-title">{playlist[int].title}</p>
+                                <p className="artist">{playlist[int].artist.name}</p>
+                            </div>
+                        </>
+                    ) : (
+                        <></>
+                    )}
+                </div>
+            </div>
+            <audio
+                autoPlay={isPlaying}
+                className="MusicPlayerAudio"
+                ref={audioRef}
+                loop={isRepeating}
+                onTimeUpdate={handleMusicPlaying}
+                onLoadedMetadata={handleLoadedMetaData}
+                onEnded={() => handleNextPrevious(1)}
+            >
+                {/* {console.log(playlist)} */}
+                <source type="audio/mp3"></source>
+                Your browser does not support audio element.
+            </audio>
+            <div className="progress-controller">
+                <div className="control-buttons">
+                    <i>
+                        <AiOutlineLike />
+                    </i>
+                    <i>
+                        <AiFillStepBackward onClick={() => handleNextPrevious(-1)} />
+                    </i>
+                    <i className="play-pause">
+                        {isPlaying ? (
+                            <AiFillPauseCircle onClick={handlePlayPause} />
+                        ) : (
+                            <AiFillPlayCircle onClick={handlePlayPause} />
+                        )}
+                    </i>
+                    <i>
+                        <AiFillStepForward onClick={() => handleNextPrevious(1)} />
+                    </i>
+                    <i>
+                        <MdReplayCircleFilled onClick={handleRepeat} />
+                    </i>
+                </div>
+                <div className="progress-container">
+                    <span>{currentTime}</span>
+                    {/* <div className="progress-bar">
+                            <div className="progress"></div>
+                        </div> */}
+                    <input
+                        type="range"
+                        value={currentTime}
+                        max={duration}
+                        step={1}
+                        onMouseDown={handleMusicMouseDown}
+                        onChange={handleMusicTimestamp}
+                        onMouseUp={handleMusicMouseUp}
+                    ></input>
+                    <span>{duration}</span>
+                </div>
             </div>
         </div>
     );
